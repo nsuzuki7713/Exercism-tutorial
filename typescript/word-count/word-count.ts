45a5ba17db1a5f228608f6481cf84c1cb6e18b29
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-06-12 00:26:48 +0900

    add answer

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -1,14 +1,35 @@
-export default class Words {
-  count(phrase: string) {
-    let arrayWords = phrase.split(/[\s]/);
-    const counts: { [key: string]: number } = {};
+// export default class Words {
+//   count(phrase: string) {
+//     let arrayWords = phrase.split(/[\s]/);
+//     const counts = new Map<string, number>();
 
-    arrayWords.forEach(v => {
-      v = v.toLowerCase();
-      if (v === '') return;
-      counts[v] = counts[v] ? counts[v] + 1 : 1;
+//     arrayWords.forEach(word => {
+//       word = word.toLowerCase();
+//       if (word === '') return;
+//       if (counts.get(word) === undefined) {
+//         counts.set(word, 0);
+//       }
+//       counts.set(word, (counts.get(word) as number) + 1);
+//     });
+
+//     return counts;
+//   }
+// }
+
+// 他の人の解答
+class Words {
+  count(s: string): Map<string, number> {
+    let words = s
+      .trim()
+      .toLowerCase()
+      .split(/\s+/);
+    let counted = new Map<string, number>();
+    words.forEach(word => {
+      let oldValue = counted.get(word) || 0;
+      counted.set(word, oldValue + 1);
     });
-
-    return new Map(Object.entries(counts));
+    return counted;
   }
 }
+
+export default Words;
