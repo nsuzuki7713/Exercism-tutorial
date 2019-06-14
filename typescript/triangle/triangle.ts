63aa515b019fef1fcd96d438499322c290190025
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-06-15 00:53:17 +0900

    リファクタリング

--- typescript/triangle/triangle.ts
+++ typescript/triangle/triangle.ts
@@ -10,22 +10,20 @@ export default class Triangle {
     const b = this.sides[1];
     const c = this.sides[2];
 
-    // 辺の長さは0以上
-    if (this.sides.some(v => v <= 0)) throw new Error();
+    // Side length is 0 or more
+    if (this.sides.some(len => len <= 0)) throw new Error();
 
-    // どの辺の長さも他の二辺の長さの和より小さい
+    // The length of any side is less than the sum of the lengths of the other two sides
     if (a + b < c || a + c < b || b + c < a) throw new Error();
 
-    // 正三角形
     if (a === b && b === c) {
       return 'equilateral';
     }
 
-    // 二等辺三角形
     if (a === b || b === c || a === c) {
       return 'isosceles';
     }
-    // 不等辺三角形
+
     return 'scalene';
   }
 }
