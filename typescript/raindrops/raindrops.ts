fc68b301806dbe4daa69db753586a28926d220fd
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-06-26 10:36:45 +0900

    add raindrops

---
+++ typescript/raindrops/raindrops.ts
@@ -0,0 +1,40 @@
+class Raindrops {
+  convert(n: number): string {
+    let result = '';
+
+    if (n % 3 === 0) {
+      result += 'Pling';
+    }
+    if (n % 5 === 0) {
+      result += 'Plang';
+    }
+    if (n % 7 === 0) {
+      result += 'Plong';
+    }
+
+    if (result === '') {
+      result = String(n);
+    }
+    return result;
+  }
+}
+
+export default Raindrops;
+
+// 他の人の解答
+// export default class Raindrops {
+//   private readonly map: { [num: number]: string } = {
+//     3: 'Pling',
+//     5: 'Plang',
+//     7: 'Plong'
+//   };
+//   convert(number: number): string {
+//     let result = '';
+//     for (let i in this.map) {
+//       if (number % Number(i) === 0) {
+//         result += this.map[i];
+//       }
+//     }
+//     return result.length !== 0 ? result : number.toString();
+//   }
+// }
